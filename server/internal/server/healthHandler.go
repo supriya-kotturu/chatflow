52d35e3e3b12b40377b88ba855899294c05cb888
supriyakotturu <kotturu.sa@northeastern.edu>
2026-02-12 12:17:39 -0800

    refactor: :recycle: Simplify HealthHandler by removing unused JSON response and timestamp logic

--- server/internal/server/healthHandler.go
+++ server/internal/server/healthHandler.go
@@ -1,21 +1,11 @@
 package internal
 
 import (
-	"encoding/json"
 	"net/http"
-	"time"
 )
 
 // HealthHandler handles health check requests and updates stats.
 func (s *Server) HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	response := map[string]interface{}{
-		"failedRequests":     s.Stats.FailedRequests.Load(),
-		"successfulRequests": s.Stats.SuccessfulRequests.Load(),
-		"timestamp":          time.Now().Unix(),
-	}
-
-	json.NewEncoder(w).Encode(response)
 }
